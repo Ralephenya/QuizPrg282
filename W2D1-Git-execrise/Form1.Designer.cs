ebd75df7e52b4419446d8a112267204be7416c00
Steve Junior Ralephenya <577023@student.belgiumcampus.ac.za>
2021-10-25 15:18:20 +0200

    Adding controllers to the form

--- W2D1-Git-execrise/Form1.Designer.cs
+++ W2D1-Git-execrise/Form1.Designer.cs
@@ -29,13 +29,44 @@ namespace W2D1_Git_execrise
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ReadBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ReadBtn
+            // 
+            this.ReadBtn.Location = new System.Drawing.Point(339, 342);
+            this.ReadBtn.Name = "ReadBtn";
+            this.ReadBtn.Size = new System.Drawing.Size(158, 63);
+            this.ReadBtn.TabIndex = 0;
+            this.ReadBtn.Text = "READ";
+            this.ReadBtn.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(143, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(626, 251);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.ReadBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ReadBtn;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
