01d94571215fd841ef14e1160cceba19919136ad
Steve Junior Ralephenya <577023@student.belgiumcampus.ac.za>
2021-10-25 15:08:58 +0200

    Add project files.

---
+++ W2D1-Git-execrise/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W2D1_Git_execrise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
